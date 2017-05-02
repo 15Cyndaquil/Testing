1932c17b330087c16c4b25d31d38f42b66499982
15Cyndaquil <john.tyler.gafford@gmail.com>
2017-05-02 13:01:19 -0400

    Finished work on ch11 prob 17 & ch17 prob 9 & ch 19 prob 10

--- src/homework/ch11/prob_17/PerfectSquare.java
+++ src/homework/ch11/prob_17/PerfectSquare.java
@@ -1,22 +1,56 @@
 package homework.ch11.prob_17;
 
-public class PerfectSquare {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-        final int runTimes = 100;
 
-        for (int i = 0; i < runTimes; i++) {
-            int m = (int) (Math.random() * 1000);
-            int n = 1;
-            double root = Math.sqrt(m * n);
+public class PerfectSquare extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Pane pane = new Pane();
+        VBox show = new VBox();
 
-            while (Math.floor(root)!=root) {
-                root = Math.sqrt(m * ++n);
+        TextField input = new TextField();
+        input.setPromptText("Input an Integer");
+
+        Button getSquare = new Button("Get the number that when multiplied by inputted number is a perfect square");
+        getSquare.setDefaultButton(true);
+        getSquare.setOnAction(event->{
+            try{
+                showSquare(Integer.valueOf(input.getText()));
+            }catch (NumberFormatException exc){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("Inputted information is not an integer");
+                alert.showAndWait();
             }
+        });
 
-            System.out.println("Inputed int: "
-                    +m+", Smallest int for perfect square: "
-                    +n+", m*n = "+(m*n));
+        show.getChildren().addAll(input, getSquare);
+        pane.getChildren().add(show);
+        primaryStage.setScene(new Scene(pane));
+        pane.requestFocus();
+        primaryStage.show();
+    }
+
+    private void showSquare(int input) {
+        int m = input;
+        int n = 1;
+        double root = Math.sqrt(m * n);
+
+        while (Math.floor(root) != root) {
+            root = Math.sqrt(m * ++n);
         }
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setContentText("Inputed int: "
+                + m + "\nSmallest int for perfect square: "
+                + n + "\nInput * Smallest int = " + (m * n));
+        alert.showAndWait();
     }
 }
