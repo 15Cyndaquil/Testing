84f39f28294e879beb3d09054cfc52326c9ac601
John Tyler Gafford <john.tyler.gafford@gmail.com>
2017-05-23 14:19:04 -0400

    Reordered classes for different assignments.

---
+++ src/homework/BookProblems/ch1/prob_2/Disp5Msg.java
@@ -0,0 +1,12 @@
+package homework.BookProblems.ch1.prob_2;
+
+/**
+ * Created by 15Cyndaquil on 5/3/2017.
+ */
+public class Disp5Msg {
+    public static void main(String[] args){
+        for(int i=0; i<5; i++){
+            System.out.println("Welcome to Java");
+        }
+    }
+}
