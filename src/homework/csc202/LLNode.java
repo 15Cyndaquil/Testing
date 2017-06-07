fc14f99e5d90d2dbabc79c07406af90540b981d5
John Tyler Gafford <john.tyler.gafford@gmail.com>
2017-06-07 14:16:30 -0400

    Create LLNode.java
    tmp in class discussion

---
+++ src/homework/csc202/LLNode.java
@@ -0,0 +1,52 @@
+
+public class LLNode {
+		private Node first, last;
+		
+		public void enQueue(int data){
+			if(isEmpty()){
+				first = new Node(data);
+				last = first;
+			}else if(first.getNode()==null){
+				first.setNode(new Node(data));
+				last = first.getNode();
+			}else{
+				last.setNode(new Node(data));
+				last = last.getNode();
+			}
+		}
+		
+		
+		public int deQueue(){
+			if(!isEmpty()){
+				int output = first.getData();
+				if(last==first){
+					first=null;
+					last=null;
+				}else{
+					first = first.getNode();
+				}
+				return output;
+			}else
+				return -1;
+		}
+
+		public int size(){
+			Node tmp = first;
+			int size=0;
+			while(tmp!=null){
+				size++;
+				tmp=tmp.getNode();
+			}
+			return size;
+		}
+		
+		public boolean isFull(){
+			return false;
+		}
+		public boolean isEmpty(){
+			if(last==null){
+				return true;
+			}else 
+				return false;
+		}
+}
